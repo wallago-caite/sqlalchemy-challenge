9ec1d1e8dc956c2fd0b390316d405a4956fad219
wallago-caite <caite.greenwood@gmail.com>
2024-06-14 19:38:46 -0500

    app.py updated with commenting

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,67 @@
 # Import the dependencies.
+from flask import Flask, jsonify
+import datetime as dt
 
+import sqlalchemy as sql
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import inspect
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = sql.create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+app.route('/')
+def home():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080, debug=True)
+
+
+###
+#LIST ALL ROUTES
+
+#/api/v1.0/0/precipitation : 
+# Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value
+# Return the JSON representation of your dictionary.
+
+
+#/api/v1.0/stations : 
+# Return a JSON list of stations from the dataset.
+
+
+#/api/v1.0/tobs : 
+#Query the dates and temperature observations of the most-active station for the previous year of data.
+#Return a JSON list of temperature observations for the previous year.
+
+#/api/v1.0/<cstart> and /api/v1.0/<cstart>/<end>
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+
